cc1c816cdf554c2096e5b1af3235b2ce6ea02a42
Apophis142 <eduard.gryaznov03@mail.ru>
2025-05-06 16:05:59 +0300

    fixed overwriting saving

--- train_nn.py
+++ train_nn.py
@@ -65,7 +65,7 @@ def train_nn(
                 eval_hist.append(eval_loss / len(test_loader))
 
             if epoch % epoch_frequency_save == 0 and epoch:
-                net.save("weights/training/", f"{net.name()}_epoch_{epoch}")
+                net.save("weights/training/", f"{filename_to_save}_epoch_{epoch}")
                 with open(f"models/training/{filename_to_save}_loss_history_epoch.pkl", 'wb+') as f:
                     pickle.dump((tuple(loss_hist), tuple(eval_hist)), f)
             torch.cuda.empty_cache()
